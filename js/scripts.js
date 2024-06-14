334745229f41286175bebf73a345f0496e5e7986
julieta548 <julietasosa548@gmail.com>
2024-06-14 14:20:04 -0300

    cambios 14/6

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
+let diapositivaActual = 0;
+
+function mostrarDiapositiva(indice) {
+    const diapositivas = document.querySelectorAll('.carrusel-item');
+    const totalDiapositivas = diapositivas.length;
+    const diapositivasPorVista = 4;
+
+    if (indice >= 6) {
+        diapositivaActual = 0;
+    } else if (indice < 0) {
+        diapositivaActual = totalDiapositivas - 1;
+    } else {
+        diapositivaActual = indice;
+    }
+
+    const desplazamiento = -diapositivaActual * 100 / diapositivasPorVista;
+    document.querySelector('.carrusel-interior').style.transform = `translateX(${desplazamiento}%)`;
+}
+
+function diapositivaSiguiente() {
+    mostrarDiapositiva(diapositivaActual + 1);
+}
+
+function diapositivaAnterior() {
+    mostrarDiapositiva(diapositivaActual - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarDiapositiva(diapositivaActual);
+});
